82632ec68306f377e3382dd8b6b86fff039d0553
Lee <60326418+JHLee1995@users.noreply.github.com>
2021-11-09 10:25:07 -0500

    Update tea-dec.c

--- tea-dec.c
+++ tea-dec.c
@@ -2,11 +2,10 @@
 #include <stdint.h>
 #include <string.h>
 
-void decrypt (uint32_t v[2], uint32_t k[4]) 
-{
-    uint32_t v0 = v[0], v1 = v[1], sum=0xC6EF3720, i;  
+void decrypt (uint32_t v[2], uint32_t k[4]) {
+    uint32_t v0 = v[0], v1 = v[1], sum = 0xC6EF3720, i;  
     uint32_t delta = 0x9E3779B9;                     
-    uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];  
+    uint32_t k0 = k[0], k1 = k[1], k2 = k[2], k3 = k[3];  
     for (i = 0; i < 32; i++) {                         
 		v1 -= ((v0 << 4) + k2) ^ (v0 + sum) ^ ((v0 >> 5) + k3);
 		v0 -= ((v1 << 4) + k0) ^ (v1 + sum) ^ ((v1 >> 5) + k1);
@@ -16,8 +15,7 @@ void decrypt (uint32_t v[2], uint32_t k[4])
     v[1]=v1;
 }
 
-int main(int argc,char **argv)
-{
+int main(int argc,char **argv) {
 	FILE* plain = fopen(argv[1],"r");
 	FILE* cipher = fopen("plaintext.bmp","w");
 
